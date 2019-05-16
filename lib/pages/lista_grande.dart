bc6d2c589d03202d3f66f8e2de5b03e54a2d5556
Vinaum8 <vinaumpt@gmail.com>
2019-05-16 20:48:09 -0300

    Primeira Rota Criada com Navigator

--- lib/pages/lista_grande.dart
+++ lib/pages/lista_grande.dart
@@ -5,6 +5,7 @@ class ListaGigante extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red,
         title: Text("Long List"),
       ),
       body: getListView(),
